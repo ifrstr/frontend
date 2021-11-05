8c58d2f8ce41f1d6c3645a3558a386addfc0144d
Il Harper <afanyiyu@hotmail.com>
2021-11-06 00:32:11 +0800

    feat(ui): add App

--- packages/frontend-ilharper/pages/_app.tsx
+++ packages/frontend-ilharper/pages/_app.tsx
@@ -3,8 +3,14 @@ import '../styles/globals.css'
 
 import type { AppProps } from 'next/app'
 
+import App from '@ifrstr/ui/components/layout/App'
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <App>
+      <Component {...pageProps} />
+    </App>
+  )
 }
 
 export default MyApp
