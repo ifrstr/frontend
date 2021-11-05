11389090e7286ac7d1653ecd5f96c79df40fbb26
Il Harper <afanyiyu@hotmail.com>
2021-11-06 00:45:54 +0800

    fix(ui): pass multiple children to App

--- packages/ui/components/layout/App.tsx
+++ packages/ui/components/layout/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-const App: React.FC<{ children: React.ReactElement<any, any> | null }> = (
-  props
-) => props.children
+const App: React.FC<{
+  children: React.ReactElement<any, any> | React.ReactElement<any, any>[] | null
+}> = (props) => <>{props.children}</>
 
 export default App
