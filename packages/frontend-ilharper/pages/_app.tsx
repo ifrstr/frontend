5f87bfceaa13ffe08555b041b84abc5d855018a3
Il Harper <afanyiyu@hotmail.com>
2021-11-06 16:55:59 +0800

    feat(ui): add Footer

--- packages/frontend-ilharper/pages/_app.tsx
+++ packages/frontend-ilharper/pages/_app.tsx
@@ -6,6 +6,7 @@ import type { AppProps } from 'next/app'
 
 import App from '@ifrstr/ui/components/layout/App'
 import Main from '@ifrstr/ui/components/layout/Main'
+import Footer from '@ifrstr/ui/components/layout/Footer'
 import NavBar from '@ifrstr/ui/components/layout/NavBar'
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -20,6 +21,15 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Main>
         <Component {...pageProps} />
       </Main>
+      <Footer
+        copyright="Hello from IlHarper Infrastructure."
+        beian={[
+          {
+            href: 'https://beian.miit.gov.cn/',
+            name: '浙ICP备2021029727号-1',
+          },
+        ]}
+      />
     </App>
   )
 }
---
+++ packages/ui/components/layout/Footer.tsx
@@ -0,0 +1,72 @@
+import Link from 'next/link'
+import { StrLink } from '../../types/StrLink'
+
+export type FooterProps = {
+  copyright?: StrLink | string
+  legalLinks?: StrLink[]
+  beian?: StrLink[]
+}
+
+const Footer: React.FC<FooterProps> = (props: FooterProps) => {
+  // Resolve props.copyright
+  let copyrightSegment: JSX.Element
+  if (props.copyright == undefined) {
+    // undefined
+    copyrightSegment = <></>
+  } else if (typeof props.copyright === 'string') {
+    // string
+    copyrightSegment = <span>{props.copyright as string}</span>
+  } else {
+    // StrLink
+    const copyright = props.copyright as StrLink
+    copyrightSegment = (
+      <Link href={copyright.href}>
+        <a>
+          <span>{copyright.name}</span>
+        </a>
+      </Link>
+    )
+  }
+
+  return (
+    <footer>
+      <div>
+        <section>
+          <div
+            style={{
+              borderBottom: '1px solid var(--str-f-p)',
+            }}
+          ></div>
+          <div>
+            {copyrightSegment && (
+              <span>
+                <div style={{ float: 'left' }}>{copyrightSegment}</div>
+              </span>
+            )}
+            {props.legalLinks &&
+              props.legalLinks.map((x) => (
+                <Link href={x.href}>
+                  <a target="_blank">
+                    <span>{x.name}</span>
+                  </a>
+                </Link>
+              ))}
+          </div>
+          {props.beian && (
+            <p>
+              {props.beian.map((x) => (
+                <>
+                  <Link href={x.href}>
+                    <a target="_blank">{x.name}</a>
+                  </Link>{' '}
+                </>
+              ))}
+            </p>
+          )}
+        </section>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
