63fc7049f49111a8bfc138188572faf625add0f5
Il Harper <afanyiyu@hotmail.com>
2021-11-16 16:09:05 +0800

    refactor(ilharper): use arrow function

--- packages/frontend-ilharper/pages/_app.tsx
+++ packages/frontend-ilharper/pages/_app.tsx
@@ -6,29 +6,27 @@ import type { AppProps } from 'next/app'
 
 import { App, Main, Footer, NavBar } from '@ifrstr/ui'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <App>
-      <NavBar
-        links={[
-          { name: 'Il Harper', href: '/' },
-          { name: 'about', href: '/about' },
-        ]}
-      />
-      <Main>
-        <Component {...pageProps} />
-      </Main>
-      <Footer
-        copyright="Hello from IlHarper Infrastructure."
-        beian={[
-          {
-            href: 'https://beian.miit.gov.cn/',
-            name: '浙ICP备2021029727号-1',
-          },
-        ]}
-      />
-    </App>
-  )
-}
+const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => (
+  <App>
+    <NavBar
+      links={[
+        { name: 'Il Harper', href: '/' },
+        { name: 'about', href: '/about' },
+      ]}
+    />
+    <Main>
+      <Component {...pageProps} />
+    </Main>
+    <Footer
+      copyright="Hello from IlHarper Infrastructure."
+      beian={[
+        {
+          href: 'https://beian.miit.gov.cn/',
+          name: '浙ICP备2021029727号-1',
+        },
+      ]}
+    />
+  </App>
+)
 
 export default MyApp
