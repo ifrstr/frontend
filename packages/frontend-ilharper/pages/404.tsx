3cf337c756b80acd6135ed85173e7f4010ac0553
Il Harper <afanyiyu@hotmail.com>
2021-12-12 00:16:45 +0800

    feat(ilharper): add 404 and 500 pages

---
+++ packages/frontend-ilharper/pages/404.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const NotFound: NextPage = () => (
+  <>
+    <Head>
+      <title>404</title>
+    </Head>
+
+    <h1>404</h1>
+    {/* <h2>昔人已乘黃鶴去&emsp;此地空餘404</h2> */}
+    <h2>Page Not Found</h2>
+  </>
+)
+
+export default NotFound
---
+++ packages/frontend-ilharper/pages/500.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const ServerError: NextPage = () => (
+  <>
+    <Head>
+      <title>500</title>
+    </Head>
+
+    <h1>500</h1>
+    <h2>Server error occurred</h2>
+  </>
+)
+
+export default ServerError
