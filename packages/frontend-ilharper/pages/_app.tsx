ee034eb9a073e41c7e450d651350f81318405281
Il Harper <afanyiyu@hotmail.com>
2021-12-12 01:11:00 +0800

    refactor(ilharper): custom app component update

--- packages/frontend-ilharper/pages/_app.tsx
+++ packages/frontend-ilharper/pages/_app.tsx
@@ -6,27 +6,30 @@ import type { AppProps } from 'next/app'
 
 import { App, Main, Footer, NavBar } from '@ifrstr/ui'
 
-const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => (
-  <App>
-    <NavBar
-      links={[
-        { name: 'Il Harper', href: '/' },
-        { name: 'about', href: '/about' },
-      ]}
-    />
-    <Main>
-      <Component {...pageProps} />
-    </Main>
-    <Footer
-      copyright="Hello from IlHarper Infrastructure."
-      beian={[
-        {
-          href: 'https://beian.miit.gov.cn/',
-          name: '浙ICP备2021029727号-1',
-        },
-      ]}
-    />
-  </App>
-)
+const MyApp = ({ Component, pageProps }: AppProps): JSX.Element =>
+  pageProps.useEmptyLayout ? (
+    <Component {...pageProps} />
+  ) : (
+    <App>
+      <NavBar
+        links={[
+          { name: 'Il Harper', href: '/' },
+          { name: 'about', href: '/about' },
+        ]}
+      />
+      <Main>
+        <Component {...pageProps} />
+      </Main>
+      <Footer
+        copyright="Hello from IlHarper Infrastructure."
+        beian={[
+          {
+            href: 'https://beian.miit.gov.cn/',
+            name: '浙ICP备2021029727号-1',
+          },
+        ]}
+      />
+    </App>
+  )
 
 export default MyApp
