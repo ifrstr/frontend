7c2b3c57f99d69cb98a75a7bf66a23ff5b17d999
Il Harper <hi@ilharper.com>
2022-06-07 10:28:50 +0800

    chore: update style

--- packages/ui/src/components/layout/Main.tsx
+++ packages/ui/src/components/layout/Main.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 const Main: React.FC<{
   children: React.ReactElement<any, any> | React.ReactElement<any, any>[] | null
-}> = (props) => <main>{props.children}</main>
+}> = (props) => <main style={{ margin: '1rem' }}>{props.children}</main>
 
 export { Main }
